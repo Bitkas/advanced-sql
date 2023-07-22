69877ed8705f05676a465c7ba7646322c389ece4
Bitkas <61255210+Bitkas@users.noreply.github.com>
2023-07-22 09:42:28 +0000

    CH_01_EP_02

---
+++ Scripts/ch_01_02.sql
@@ -0,0 +1,6 @@
+select emp.firstName, emp.lastName, sls.salesId 
+from employee emp 
+LEFT JOIN sales sls 
+ON emp.employeeId = sls.employeeId 
+WHERE emp.title = 'Sales Person'
+AND sls.salesId IS NULL; 
